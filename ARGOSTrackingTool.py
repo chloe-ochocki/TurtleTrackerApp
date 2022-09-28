00ff5bb0772a253101db2dddfbd2ab1fccdaed15
chloe-ochocki <112418725+chloe-ochocki@users.noreply.github.com>
2022-09-28 12:50:56 -0400

    Create ARGOSTrackingTool.py
    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Chloe Ochocki (chloe.ochocki@duke.edu)
+# Date:   Fall 2022 (9/28/22)
+#--------------------------------------------------------------
